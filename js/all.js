7ec4e3cba3244bd3f29ad4a22aa99d73b2ec9e1d
aa367421 <74287448+aa367421@users.noreply.github.com>
2022-09-21 00:13:19 +0800

    0920

--- js/all.js
+++ js/all.js
@@ -1,3 +1,5 @@
+
+// Nav bar hover
 const list = document.querySelector('.list');
 const listContent = document.querySelectorAll('.list li a');
 
@@ -13,23 +15,38 @@ list.addEventListener('mouseleave', () => {
     })
 })
 
+
+// Nav bar click
 $(document).ready(function(){
-    $('.list li a').click(function(event){
+    $('.list li a, .title a').click(function(event){
         let target = $(this).attr('href');
         let t = target.split('#');
         event.preventDefault();
 
+        let ZoomAndLocate = (sec) => {
+            let obj = document.querySelector(`#${t[1]} + *`);
+            setTimeout(() => {
+                obj.classList.add('active');
+            }, sec * 1000)
+            setTimeout(() => {
+                window.location.href=`${t[1]}.html`;
+            }, sec * 2000)
+        }
+
         if (t[1] === "banner"){
             t[1] = 'index';
         }
-
+        // if @ homepage -> $(target).offset == true
         if ($(target).offset()){
-            $('html, body').animate({
-                scrollTop: $(target).offset().top - 75,
-            }, 700);
-            setTimeout(() => {
-                window.location.href=`${t[1]}.html`;
-            }, 1500)
+            // if @ nav bar -> parent node == a -> scroll
+            if ($(this)[0].parentNode.nodeName !== 'H2'){
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top - 90,
+                }, 700);
+                ZoomAndLocate(1);
+            } else {
+                ZoomAndLocate(0.5);
+            }
         } else {
             window.location.href=`${t[1]}.html`;
         }
@@ -37,33 +54,118 @@ $(document).ready(function(){
     })
 })
 
+
+// Scroll
+window.addEventListener('scroll', () =>{
+    checkScrollPos();
+})
+
 let card = document.querySelectorAll('.card li');
 let about = document.querySelectorAll('.aboutContent, .about>img');
 let contact = document.querySelectorAll('.contact>img, .contactContent')
 
-window.addEventListener('scroll', () =>{
+const checkScrollPos = () => {
     let windowHeight = window.innerHeight;
     let scrollPos = window.scrollY;
-    // console.log(scrollPos + windowHeight);
-    if ((windowHeight + scrollPos) > 428){
+    if (((windowHeight + scrollPos) /2 ) > 500){
         card.forEach((item, index) =>{
             setTimeout(() => {
                 item.classList.add('active')
-            }, index * 250);
+            }, index * 150);
         })
     }
-    if ((windowHeight + scrollPos) > 1400){
+    if (((windowHeight + scrollPos) /2 ) > 800){
         about.forEach((item, index) =>{
             setTimeout(() => {
                 item.classList.add('active')
-            }, index * 250);
+            }, index * 150);
         })
     }
-    if ((windowHeight + scrollPos) > 1700){
+    if (((windowHeight + scrollPos) /2 ) > 1000){
         contact.forEach((item, index) =>{
             setTimeout(() => {
                 item.classList.add('active')
-            }, index * 250);
+            }, index * 150);
         })
+    }
+}
+
+// home card click
+
+const cardList = document.querySelector('.card');
+
+if (cardList !== null){
+    cardList.addEventListener('click', (e) => {
+        let el = e.target.closest('li');
+        el.classList.add('locating');
+
+        setTimeout(() => {
+            window.location.href=`work.html?now=${el.dataset.id}`;
+        }, 700)
+    })
+}
+
+
+let viewStatus = 'all';
+// workbtn
+const buttonGroup = document.querySelector('.btnGroup');
+const btns = document.querySelectorAll('.btnGroup form button');
+const workCards = document.querySelectorAll('.workCard li');
+
+const checkWork = () => {
+    let url = new URL(location.href);
+        let now = url.searchParams.get('now');
+        if (now !== null){
+            viewStatus = now;
+            workCards.forEach(item => {
+                if (item.dataset.id !== viewStatus){
+                    item.classList.add('unseen');
+                }
+            })
+            btns.forEach(item => {
+                if(item.value === viewStatus){
+                    item.classList.add('active');
+                }
+            })
+        } else {
+            return;
+        }
+}
+
+// render after f5
+checkScrollPos();
+checkWork();
+
+// workbtn click
+buttonGroup.addEventListener('click', (e) => {
+    if (e.target.nodeName === "BUTTON"){
+        e.preventDefault();
+
+        // reset viewStatus
+        workCards.forEach(item => {
+            item.classList.remove('unseen');
+        })
+        // toggle class
+        if (!e.target.classList.contains('active')){
+            let ary = Array.from(btns);
+            ary.forEach(item => {
+                item.classList.remove('active');
+            })
+
+            ary = ary.filter(item => {
+            if (item.classList.contains('active')){
+                return true
+                }
+            })
+
+            // change viewStatus
+            viewStatus = e.target.value;
+            workCards.forEach(item => {
+                if (item.dataset.id !== viewStatus){
+                    item.classList.add('unseen');
+                }
+            })
+        }
+        e.target.classList.toggle('active');
     }
 })
