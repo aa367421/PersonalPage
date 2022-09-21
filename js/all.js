11fe924d92c352803cff1baba3b56bde8e3a541f
aa367421 <74287448+aa367421@users.noreply.github.com>
2022-09-22 02:33:24 +0800

    0921
    cursor, json call ( commented )

--- js/all.js
+++ js/all.js
@@ -43,7 +43,11 @@ $(document).ready(function(){
                 $('html, body').animate({
                     scrollTop: $(target).offset().top - 90,
                 }, 700);
-                ZoomAndLocate(1);
+                if (t[1] === 'index'){
+                    return
+                } else {
+                    ZoomAndLocate(1);
+                }
             } else {
                 ZoomAndLocate(0.5);
             }
@@ -56,37 +60,36 @@ $(document).ready(function(){
 
 
 // Scroll
-window.addEventListener('scroll', () =>{
-    checkScrollPos();
-})
-
 let card = document.querySelectorAll('.card li');
 let about = document.querySelectorAll('.aboutContent, .about>img');
 let contact = document.querySelectorAll('.contact>img, .contactContent')
+let homeTitle = document.querySelectorAll('.title');
+let [workTitle, aboutTitle, contactTitle] = homeTitle;
 
 const checkScrollPos = () => {
     let windowHeight = window.innerHeight;
     let scrollPos = window.scrollY;
-    if (((windowHeight + scrollPos) /2 ) > 500){
-        card.forEach((item, index) =>{
+    let posCount = windowHeight /1.5 + scrollPos;
+    // console.log(windowHeight);//929 714
+    // console.log(scrollPos);
+    // console.log(workTitle.offsetTop);//769 633
+    // console.log(aboutTitle.offsetTop);//1169 1033
+    // console.log(contactTitle.offsetTop);//1712 1486
+    let contentPopUp = (nodeName) => {
+        nodeName.forEach((item, index) =>{
             setTimeout(() => {
                 item.classList.add('active')
             }, index * 150);
         })
     }
-    if (((windowHeight + scrollPos) /2 ) > 800){
-        about.forEach((item, index) =>{
-            setTimeout(() => {
-                item.classList.add('active')
-            }, index * 150);
-        })
+    if (posCount > workTitle.offsetTop){
+        contentPopUp(card);
     }
-    if (((windowHeight + scrollPos) /2 ) > 1000){
-        contact.forEach((item, index) =>{
-            setTimeout(() => {
-                item.classList.add('active')
-            }, index * 150);
-        })
+    if (posCount > aboutTitle.offsetTop){
+        contentPopUp(about);
+    }
+    if (posCount > contactTitle.offsetTop){
+        contentPopUp(contact);
     }
 }
 
@@ -100,17 +103,40 @@ if (cardList !== null){
         el.classList.add('locating');
 
         setTimeout(() => {
-            window.location.href=`work.html?now=${el.dataset.id}`;
+            location.href=`work.html?now=${el.dataset.id}`;
         }, 700)
     })
 }
 
 
 let viewStatus = 'all';
+
 // workbtn
 const buttonGroup = document.querySelector('.btnGroup');
 const btns = document.querySelectorAll('.btnGroup form button');
-const workCards = document.querySelectorAll('.workCard li');
+const workCardList = document.querySelector('.workCard');
+let workCard;
+
+// get json
+// let jsonURL = 'json/test.json';
+// let request = new XMLHttpRequest();
+// request.open('GET', jsonURL);
+// request.responseType= 'json';
+// request.send();
+
+// request.onload = () => {
+//     let ary = request.response;
+//     let obj = ary[0];
+//     let {type, titilImg, typeText} = obj;
+//     workCardList.innerHTML = `
+//                 <li data-type="${type}">
+//                     <img src="./img/workCard/${titilImg}.jpg" alt="">
+//                     <p>${typeText}</p>
+//                 </li>`;
+//     workCards = document.querySelectorAll('.workCard li');
+// 
+    // checkWork();
+// }
 
 const checkWork = () => {
     let url = new URL(location.href);
@@ -118,7 +144,7 @@ const checkWork = () => {
         if (now !== null){
             viewStatus = now;
             workCards.forEach(item => {
-                if (item.dataset.id !== viewStatus){
+                if (item.dataset.type !== viewStatus){
                     item.classList.add('unseen');
                 }
             })
@@ -133,39 +159,114 @@ const checkWork = () => {
 }
 
 // render after f5
-checkScrollPos();
+if (card.length !== 0){
+    checkScrollPos();
+    window.addEventListener('scroll', () =>{
+        checkScrollPos();
+    })
+}
 checkWork();
 
 // workbtn click
-buttonGroup.addEventListener('click', (e) => {
-    if (e.target.nodeName === "BUTTON"){
-        e.preventDefault();
+// buttonGroup.addEventListener('click', (e) => {
+//     if (e.target.nodeName === "BUTTON"){
+//         e.preventDefault();
 
-        // reset viewStatus
-        workCards.forEach(item => {
-            item.classList.remove('unseen');
-        })
-        // toggle class
-        if (!e.target.classList.contains('active')){
-            let ary = Array.from(btns);
-            ary.forEach(item => {
-                item.classList.remove('active');
-            })
+//         // reset viewStatus
+//         workCards.forEach(item => {
+//             item.classList.remove('unseen');
+//         })
+//         // toggle class
+//         if (!e.target.classList.contains('active')){
+//             let ary = Array.from(btns);
+//             ary.forEach(item => {
+//                 item.classList.remove('active');
+//             })
 
-            ary = ary.filter(item => {
-            if (item.classList.contains('active')){
-                return true
-                }
-            })
+//             ary = ary.filter(item => {
+//             if (item.classList.contains('active')){
+//                 return true
+//                 }
+//             })
 
-            // change viewStatus
-            viewStatus = e.target.value;
-            workCards.forEach(item => {
-                if (item.dataset.id !== viewStatus){
-                    item.classList.add('unseen');
-                }
-            })
+//             // change viewStatus
+//             viewStatus = e.target.value;
+//             workCards.forEach(item => {
+//                 if (item.dataset.type !== viewStatus){
+//                     item.classList.add('unseen');
+//                 }
+//             })
+//         }
+//         e.target.classList.toggle('active');
+//     }
+// })
+
+
+
+// workCardList.addEventListener('click', (e) => {
+//     if (e.target.closest('li')){
+//         location.href=`work.html#lightbox`;
+//     }
+// //     request.open('GET', jsonURL);
+// //     request.responseType= 'json';
+// //     request.send();
+// //     request.onload = () => {
+// //         let ary = request.response;
+// //         let obj = ary[0].description;
+// //         let {title, job, time, content} = obj;
+// //         let lightbox = document.querySelector('.lightbox');
+// //         lightbox.innerHTML = `
+// //             <div class="lightboxContent">
+// //                 <div class="lightboxImg">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <img src="./img/CardImg.jpg" alt="">
+// //                     <!-- 燈箱JS -->
+// //                 </div>
+// //                 <div class="lightboxText">
+// //                     <h3>${title}</h3>
+// //                     <span>${job} ${time}</span>
+// //                     <p>${content}</p>
+// //                 </div>
+// //             </div>`;
+// //         location.href=`work.html#lightbox`;
+// //     }
+// })
+
+// cursor effect
+const cursorDiv = document.createElement('div');
+cursorDiv.classList.add('cursor');
+document.body.appendChild(cursorDiv);
+
+let a;
+
+const move = () => {
+        cursorDiv.style.left = `${a.x - cursorDiv.offsetWidth /2}px`;
+        cursorDiv.style.top = `${a.y - cursorDiv.offsetHeight /2}px`;
+}
+
+document.body.addEventListener('mousemove', (e) => {
+    a = e;
+    window.requestAnimationFrame(move);
+    if (e.target.closest('h2 a, li,button')){
+        if (!cursorDiv.classList.contains('active')){
+            cursorDiv.classList.add('active');
+        } else {
+            return;
         }
-        e.target.classList.toggle('active');
+    } else {
+        if (cursorDiv.classList.contains('active')){
+            cursorDiv.classList.remove('active');
+        } else {
+            return;
+        }
     }
+    
+
+
 })
